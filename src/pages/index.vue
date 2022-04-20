52727aab681aebdde3278585418d786ea40d94fa
t-east <azmtomya.a127@gmail.com>
2022-04-21 00:27:35 +0900

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <AtomButton>aa</AtomButton>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AtomButton from '~/components/atoms/AtomButton.vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {
+    AtomButton
+  }
+})
+</script>
---
+++ src/pages/parts-list.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="px-12">
+    <div class="my-2">
+      <h>AtomButton</h>
+      <AtomButton>aa</AtomButton>
+    </div>
+    <div class="my-2">
+      <h>AtomButton</h>
+      <AtomButton color="red">
+        red
+      </AtomButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AtomButton from '~/components/atoms/AtomButton.vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {
+    AtomButton
+  }
+})
+</script>
