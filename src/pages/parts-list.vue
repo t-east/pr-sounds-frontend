1e81af677a8d77884f28be8fceccbcada050852c
t-east <azmtomya.a127@gmail.com>
2022-04-21 00:49:34 +0900

    init parts-list

--- src/pages/parts-list.vue
+++ src/pages/parts-list.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="px-12">
-    <div class="my-2">
-      <h>AtomButton</h>
-      <AtomButton>aa</AtomButton>
-    </div>
-    <div class="my-2">
-      <h>AtomButton</h>
-      <AtomButton color="red">
-        red
-      </AtomButton>
+  <div class="flex justify-center">
+    <div class="py-16 px-24">
+      <div class="my-8">
+        <h1 class="text-4xl font-bold my-2">
+          Atoms
+        </h1>
+        <div class="my-2">
+          <h1>AtomButton</h1>
+          <AtomButton>button</AtomButton>
+        </div>
+        <div class="my-2">
+          <h1 class="text-red-600">
+            AtomButton
+          </h1>
+          <AtomButton color="red">
+            red
+          </AtomButton>
+        </div>
+      </div>
+      <div class="my-8">
+        <h1 class="text-4xl font-bold my-2">
+          Molecules
+        </h1>
+      </div>
     </div>
   </div>
 </template>
